36a75131dbb98204dbe8cc3740dc7ec62e1bc844
Atul16raj <atul72210@gmail.com>
2024-07-14 13:26:39 +0530

    Hii

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const Name = "Atul RAj";
+
+  return (
+    <div>
+      <h1 style={{
+        color:"red",
+        textAlign : "center",
+      }}>This is {Name}</h1>
+    </div>
+    
+  );
+}
+
+export default App;
